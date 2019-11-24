42c057307a8fb7c29775573013fbc41f2ec76db1
Akhil Raj <akhilrajmailbox@gmail.com>
2019-11-24 11:33:35 +0530

    my rsesume configuration @ Sun Nov 24 11:33:35 IST 2019

--- installer.sh
+++ installer.sh
@@ -3,24 +3,42 @@
 
 export APP_NAME=test-cv
 
-## installing the dependencies
-apt-get update && apt-get install git vim sudo curl make apache2 -y \
-    && curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - \
-    && apt install nodejs -y
+function initialsetup () {
+    ## installing the dependencies
+    apt-get update && apt-get install git vim sudo curl make apache2 -y \
+        && curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - \
+        && apt install nodejs -y
 
 
-APP_NAME=${APP_NAME} && git clone https://github.com/akhilrajmailbox/${APP_NAME}.git && cd ${APP_NAME}
+    APP_NAME=${APP_NAME} && git clone https://github.com/akhilrajmailbox/${APP_NAME}.git && cd ${APP_NAME}
 
-npm install --save gulp bower gulp-token-replace jimp terser noty -g \
-    && npm install --save popper.js@1.12.9 jquery@1.9.1 bootstrap@4.0.0
-
-make setup
-make build
+    npm install --save gulp bower gulp-token-replace jimp terser noty -g \
+        && npm install --save popper.js@1.12.9 jquery@1.9.1 bootstrap@4.0.0
+}
 
 
-## restarting the apache2 services
-/etc/init.d/apache2 stop
-rm -rf /var/www/html/*
-cp -r webroot/* /var/www/html/
-/etc/init.d/apache2 start
-/etc/init.d/apache2 status
+function artifactbuild() {
+    make setup
+    make build
+}
+
+function webservice() {
+    ## restarting the apache2 services
+    /etc/init.d/apache2 stop
+    rm -rf /var/www/html/*
+    cp -r webroot/* /var/www/html/
+    /etc/init.d/apache2 start
+    /etc/init.d/apache2 status
+}
+
+
+if [[ ${1} == build ]] ; then
+    initialsetup
+    artifactbuild
+    webservice
+elif [[ ${1} == rebuild ]] ; then
+    artifactbuild
+    webservice
+else
+    echo "no input match"
+fi
