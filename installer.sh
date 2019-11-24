e90a0f4414f81cf8282a586773a8ddade8a67c47
Akhil Raj <akhilrajmailbox@gmail.com>
2019-11-24 11:25:19 +0530

    my rsesume configuration @ Sun Nov 24 11:25:19 IST 2019

--- installer.sh
+++ installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export APP_NAME=MyResume
+## apt-get update && apt-get install -y curl
+
+export APP_NAME=test-cv
 
 ## installing the dependencies
 apt-get update && apt-get install git vim sudo curl make apache2 -y \
@@ -7,7 +9,7 @@ apt-get update && apt-get install git vim sudo curl make apache2 -y \
     && apt install nodejs -y
 
 
-APP_NAME=cv && git clone https://github.com/akhilrajmailbox/${APP_NAME}.git && cd ${APP_NAME}
+APP_NAME=${APP_NAME} && git clone https://github.com/akhilrajmailbox/${APP_NAME}.git && cd ${APP_NAME}
 
 npm install --save gulp bower gulp-token-replace jimp terser noty -g \
     && npm install --save popper.js@1.12.9 jquery@1.9.1 bootstrap@4.0.0
