60aba4bd14414844337ad19df756562ee22a2d19
Akhil Raj <akhilrajmailbox@gmail.com>
2020-01-05 15:26:17 +0530

    updated

--- docs/assets/js/front/index.js
+++ docs/assets/js/front/index.js
@@ -1,6 +1,6 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
  * Project:  cv@0.2.4 - Modern CV, Resume and Portfolio website
- * Homepage: https://github.com/akhilrajmailbox/cv
+ * Homepage: https://github.com/akhilrajmailbox/MyResume
  * License:  MIT
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
  */
